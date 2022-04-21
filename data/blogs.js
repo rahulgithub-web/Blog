fd26cc5acda1854bc4779d886ffb166a5e040b8d
rahul <rahulbarnwalonlyu2@gmail.com>
2022-04-21 15:42:12 +0530

    first commit

---
+++ data/blogs.js
@@ -0,0 +1,34 @@
+blogs = [
+  {
+    title: "How to get statred with Express",
+    content: "This is official content",
+    slug: "express-learn",
+  },
+  {
+    title: "How to get statred with MondoDB",
+    content: "This is official content",
+    slug: "MongoDB-learn",
+  },
+  {
+    title: "How to get statred with Javascript",
+    content: "This is official content",
+    slug: "Javascript-learn",
+  },
+  {
+    title: "How to get statred with HTML",
+    content: "This is official content",
+    slug: "HTML-learn",
+  },
+  {
+    title: "How to get statred with CSS",
+    content: "This is official content",
+    slug: "CSS-learn",
+  },
+  {
+    title: "How to get statred with Reactjs",
+    content: "This is official content",
+    slug: "Reactjs-learn",
+  },
+];
+
+module.exports = blogs;
